594c5de8d19840bb024510f504bf5c85856bd896
adrianj93 <adrianj93@gmail.com>
2015-11-15 11:55:25 +0100

    Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,23 @@ var concat = require('gulp-concat');
 var angularFilesort = require('gulp-angular-filesort'),
     inject = require('gulp-inject');
 
-// define tasks here
-gulp.task('default', function() {
-    // run tasks here
-    // set up watch handlers here
-    
+gulp.task('default', function () {
+  gulp.src(['lib/angular.js', 'lib/angular-ui-router.js', 'src/app.js', 'src/ctrl/*.js', 'src/filters/*.js'])
+    .pipe(concat('build.js'))
+    .pipe(angularFilesort())
+    .pipe(gulp.dest('./'))
 });
 
-gulp.src('./index.html')
-  .pipe(inject(
-    gulp.src(['./src/**/*.js']).pipe(angularFilesort())
-  ))
-  .pipe(gulp.dest('./build'));
+gulp.watch('src/*', ['default']);
+gulp.watch('pages/*', ['default']);
 
-
-gulp.task('concat', function() {
-    gulp.src('part*')
-        .pipe(concat('result.txt'))
-        .pipe(gulp.dest('./'));
+var validate = require('gulp-html-angular-validate');
+ 
+gulp.task('html-lint', function () {
+  var options = {
+    customattrs: ['*'],
+    customtags: ['*'],
+  };
+  gulp.src('htmlGlob')
+    .pipe(validate(options));
 });
