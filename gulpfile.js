dc9f20d4aa9c931d72063df81e0f87fdedf7040a
adrianj93 <adrianj93@gmail.com>
2015-11-15 12:08:37 +0100

    gulp with server

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,14 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var angularFilesort = require('gulp-angular-filesort'),
     inject = require('gulp-inject');
+var run = require('gulp-run');
 
 gulp.task('default', function () {
   gulp.src(['lib/angular.js', 'lib/angular-ui-router.js', 'src/app.js', 'src/ctrl/*.js', 'src/filters/*.js'])
     .pipe(concat('build.js'))
     .pipe(angularFilesort())
-    .pipe(gulp.dest('./'))
+    .pipe(gulp.dest('./'));
+  run('node server/server.js').exec()
 });
 
 gulp.watch('src/*', ['default']);
